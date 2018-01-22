93279cf8ac602391f543be4ca299bd993333a8eb
Nikolay Koshenkov <23371089+nuqz@users.noreply.github.com>
2018-01-22 13:15:33 +0300

    Do not use predefined dataset sizes

--- consensus/ethash/algorithm_go1.8.go
+++ consensus/ethash/algorithm_go1.8.go
@@ -27,9 +27,6 @@ import "math/big"
 func cacheSize(block uint64) uint64 {
 	// If we have a pre-generated value, use that
 	epoch := int(block / epochLength)
-	if epoch < len(cacheSizes) {
-		return cacheSizes[epoch]
-	}
 	// No known cache size, calculate manually (sanity branch only)
 	size := cacheInitBytes + cacheGrowthBytes*uint64(epoch) - hashBytes
 	for !new(big.Int).SetUint64(size / hashBytes).ProbablyPrime(1) { // Always accurate for n < 2^64
@@ -45,9 +42,6 @@ func cacheSize(block uint64) uint64 {
 func datasetSize(block uint64) uint64 {
 	// If we have a pre-generated value, use that
 	epoch := int(block / epochLength)
-	if epoch < len(datasetSizes) {
-		return datasetSizes[epoch]
-	}
 	// No known dataset size, calculate manually (sanity branch only)
 	size := datasetInitBytes + datasetGrowthBytes*uint64(epoch) - mixBytes
 	for !new(big.Int).SetUint64(size / mixBytes).ProbablyPrime(1) { // Always accurate for n < 2^64
